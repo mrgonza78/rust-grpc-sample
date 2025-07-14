be4c391a9f08282bb9427d3e0cbee74c592fbed0
Alejandro Gonzalez <mrgonza78@gmail.com>
2025-07-14 15:22:40 -0300

    Adding grpc-web support

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ mod service;
 use proto::chat_package::chat_service_server::ChatServiceServer;
 use service::MyChatService;
 use std::error::Error;
-use tonic::transport::Server;
+use tonic::{service::LayerExt, transport::Server};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let addr = "0.0.0.0:8080".parse().unwrap();
     println!("Server listening on {addr}");
 
-    Server::builder()
-        .add_service(ChatServiceServer::new(MyChatService::new()))
+    let chat_service = tower::ServiceBuilder::new()
+        .layer(tower_http::cors::CorsLayer::permissive()) // Allowing CORS
+        .layer(tonic_web::GrpcWebLayer::new()) // Using GrpcWeb layer for the react app
+        .into_inner()
+        .named_layer(ChatServiceServer::new(MyChatService::new()));
+
+    Server::builder()        
+        .accept_http1(true) // GrpcWeb is over http1 so we must enable it.
+        .add_service(chat_service)
         .serve(addr)
         .await?;
 
